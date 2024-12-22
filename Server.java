6f43fba3e7570d0a1f3cd98414dc4a7599b807af
KzXian <steven.yong0423@gmail.com>
2024-12-22 18:33:28 +0800

    server後端更新

--- Server.java
+++ Server.java
@@ -2,9 +2,13 @@ import java.io.*;
 
 
 public class Server implements Comm.TcpServerCallback {
+  private int readyCount = 0;
+  
   public static void main(String[] args) {
     try {
       Server server = new Server();
+      // 從這裡開始遊戲(即等待所有玩家準備完成後，伺服器會告訴前端準備完成)
+      server.waitGameStart();
     } catch (IOException e) {
       System.out.println("Failed to create server: " + e.getMessage());
     }
@@ -14,14 +18,17 @@ public class Server implements Comm.TcpServerCallback {
   public Server()
     throws IOException
   {
-    server = new Comm.TcpServer(8080, 2, this);
+    server = new Comm.TcpServer(8080, 4, this);
   }
 
   @Override
   public void onMessage(int id, String message) {
-    System.out.println("Client " + id + " sent: " + message);
-    server.send(id, "Echo: " + message);
-    server.broadcast("move;player" + id + ";up");
+    if (message.startsWith("updateReadyState")) {
+      updateReadyState(message, id);
+    } 
+
+    // System.out.println("Client " + id + " sent: " + message);
+    // server.send(id, "Echo: " + message);
   }
 
   @Override
@@ -32,5 +39,19 @@ public class Server implements Comm.TcpServerCallback {
   @Override
   public void onDisconnect(int id) {
     System.out.println("Client disconnected: " + id);
+  }
+
+  void waitGameStart () {
+    // 檢查是否所有玩家都準備遊戲
+    while (readyCount != 4);
+    // 開始遊戲，告訴前端遊戲開始
+    server.broadcast("gameStart");
+  }
+
+  void updateReadyState (String message, int id) {
+    // 判斷能不能準備，此時收到的訊息封包大概是updateReadyState;ready;is_killer
+    String[] parts = message.split(";");
+    server.broadcast("ready;" + id);
+    readyCount++;
   }
 }
