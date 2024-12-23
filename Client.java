1336db554a468fd0244e97f69cf58be6cd689dd9
KzXian <steven.yong0423@gmail.com>
2024-12-23 17:35:46 +0800

    tmp3

--- Client.java
+++ Client.java
@@ -19,9 +19,8 @@ public class Client implements Comm.TcpClientCallback {
     Scanner scanner = new Scanner(System.in);
 
     // 等待伺服器連接成功後開始接受指令
-    while (client.isAlive()) {
-        setupGUI initialGUI = new setupGUI();
-    }
+    while (client.isAlive());
+    setupGUI initialGUI = new setupGUI();
 
     scanner.close();
   }
