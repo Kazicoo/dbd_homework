e7aad6548cbf2efb7ebaf0b5343f7e5ae7eb026f
KzXian <steven.yong0423@gmail.com>
2024-12-24 20:41:39 +0800

    幫檢測updateReadyState換了一個格式

--- Client.java
+++ Client.java
@@ -27,16 +27,13 @@ public class Client implements Comm.TcpClientCallback {
   @Override
   public void onMessage(String message) {
 
-    // String[] parts = message.split(";");
-    // 按下更新的角色按鈕後，會獲得updateReadyState;ready;p1;0
+    String[] parts = message.split(";");
+    // 按下更新的角色按鈕後，會獲得 updateReadyState;ready;p1;0 的封包
     // 主視窗的更新畫面
-    // if ("updateReadyState".equals(parts[0])) {
-    //   if ("ready".equals(parts[1])) {
-    //     initialGUI.playerReady(true);
-    //   } else if ("ready".equals(parts[1])) {
-    //     initialGUI.playerReady(false);
-    //   }
-    // } 
+    if (message.startsWith("updateReadyState")) {
+      if ("ready".equals(parts[1])) initialGUI.playerReady(true);
+      if ("unready".equals(parts[1])) initialGUI.playerReady(false);
+    } 
 
     System.out.println("Server sent: " + message);
   }
