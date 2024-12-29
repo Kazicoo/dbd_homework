ce80b5e6ed507c4fee73c889306f5792495ffaf0
Rex <tcmt5685@gmail.com>
2024-12-29 15:13:35 +0800

    git push

---
+++ Player.java
@@ -0,0 +1,22 @@
+public class Player {
+    private int playerId;          
+    private int relativeLocation;   
+
+    public Player(int id) {
+        this.playerId = id;
+        this.relativeLocation = -1; 
+    }
+
+    public void setRelativeLocation(int location) {
+        this.relativeLocation = location;
+    }
+
+    public int getRelativeLocation() {
+        return this.relativeLocation;
+    }
+
+    @Override
+    public String toString() {
+        return "Player{id=" + playerId + ", relativeLocation=" + relativeLocation + "}";
+    }
+}
--- ServerGame.java
+++ ServerGame.java
@@ -1,3 +1,34 @@
+import java.util.Random;
+
 public class ServerGame {
-    
+    private int[] area = {0,1,2,3,4,5,6,7,8};
+    private Player players[] = new Player[4];
+    Random rand = new Random();
+
+    public void loadingPlayerLocation(){
+        int count = 0;
+
+        players[0] = new Killer();
+
+        for(int i=1; i<4; i++){
+            players[i] = new Human();
+        }
+
+        while(count < 4){
+            int relativeLocation = rand.nextInt(9);
+            boolean isValid = true;
+
+            for(int i = 0; i < count; i++){
+                if(players[i].relativeLocation == relativeLocation){
+                    isValid = false;
+                    break;
+                }
+            }
+
+            if(isValid){
+                players[count].relativeLocation = relativeLocation;
+                count ++;
+            }
+        }
+    }
 }
