6d84b60143fc040affb88fd9dfcc40d202cc5899
KzXian <steven.yong0423@gmail.com>
2025-01-07 00:18:15 +0800

    牆壁生成

--- Client.java
+++ Client.java
@@ -114,7 +114,7 @@ public class Client implements Comm.TcpClientCallback {
     if(message.startsWith("animated")) {
       if(parts[1].equals("attack"))  
       ClientGame.attackFacing(message);
-      if(parts[1].equals("animated"))
+      if(parts[1].equals("animated")) {}
       ClientGame.moveAnimation(message);
     }
 
